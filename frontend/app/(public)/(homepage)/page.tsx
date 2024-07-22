0f537fc11ddc1fa68196d4e79932f1aa2ebf119a
ALUKU GODRICK <58194640+aluku7-wq@users.noreply.github.com>
2024-07-22 19:35:01 +0300

    m

--- frontend/app/(public)/(homepage)/page.tsx
+++ frontend/app/(public)/(homepage)/page.tsx
@@ -2,7 +2,9 @@ import Header from "@/components/header/Header";
 import React from "react";
 
 const page = () => {
-    return <div className="flex flex-col"></div>;
+    return (
+        <div className="flex justify-center h-screen items-center">pushfi</div>
+    );
 };
 
 export default page;
