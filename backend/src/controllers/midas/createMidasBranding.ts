8dc763699ccf924cc2dd683fadf19b8ae31ed04e
ALUKU GODRICK <58194640+aluku7-wq@users.noreply.github.com>
2024-07-30 23:31:36 +0300

    added midas branding logic

--- backend/src/controllers/midas/createMidasBranding.ts
+++ backend/src/controllers/midas/createMidasBranding.ts
@@ -74,7 +74,7 @@ const createMidasBranding: RequestHandler = async (req, res, next) => {
 
             return res.status(200).json(newListing);
         } catch (error) {
-            console.error("Database error:", error);
+            console.log(error);
             return res.status(500).json({
                 message: "Error saving data to database",
             });
